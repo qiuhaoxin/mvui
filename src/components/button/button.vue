7cf2cc675bf77fe130e1d5381e57087231ca9a81
qiuhaoxin <502967244@qq.com>
2018-02-28 23:36:46 +0800

    commit

--- src/components/button/button.vue
+++ src/components/button/button.vue
@@ -1,11 +1,28 @@
 <template>
    <button :type="buttonType" :class="classes" :disabled="disabled" 
    @click="handleClick">
-        <Icon class="mvui" ></Icon>
+        <Icon class="mvui" v-if='loading'></Icon>
    </button>
 </template>
 <script type='text/javascript'>
      import Icon from '../Icon';
 
+     export default{
+        name:'Button',
+        components:['Icon'],
+        props:{
 
+        },
+        data(){
+            return {
+            
+            }
+        },
+        methods{
+           handleClick(){
+
+           }
+        },
+
+     }
 </script>
