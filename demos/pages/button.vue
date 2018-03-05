c12c4a62d8dcf9e010c68a7fb974263d46e43794
qiuhaoxin <502967244@qq.com>
2018-03-05 20:07:55 +0800

    commit

--- demos/pages/button.vue
+++ demos/pages/button.vue
@@ -3,6 +3,10 @@
          <Button>Default</Button>
 
          <Button type="primary">Primary</Button>
+         <Button type="ghost">Ghost</Button>
+         <Button type="dashed">Dashed</Button>
+
+         <Button type="primary" shape="circle">Circle</Button>
      </div>
 </template>
 <scipt>
